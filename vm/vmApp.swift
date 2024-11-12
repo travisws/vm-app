d56265894295d8f85b715de5ea651b3b0f81c273
travisws <traviswoodworthsmith@gmail.com>
2024-11-11 23:08:56 -0700

    Initial Commit

---
+++ vm/vmApp.swift
@@ -0,0 +1,17 @@
+//
+//  vmApp.swift
+//  vm
+//
+//  Created by deathpoolops on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct vmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
